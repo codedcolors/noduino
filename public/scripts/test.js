1d87f743324f22ce10c52709f2af1b7bbdc693c5
Left Field Labs <lfl@edam.local>
2013-01-03 16:59:55 -0800

    Refactored some of the var names used in test.js.

--- public/scripts/test.js
+++ public/scripts/test.js
@@ -1,4 +1,44 @@
 require(["jquery"], function($) {
+
+  var getOnBoardConnectionCallback = function() {
+    return function(err, board) {
+
+      $('#e2-exampleConnection .alert').addClass('hide');
+
+      if (err) {
+        $('.board-error').removeClass('hide');
+        console.log("Error connecting to board.");
+      } else {
+
+        console.log("Success connecting to board.");
+
+        // Start blinking.
+        var that = this;
+        if (!that.led) {
+          board.withLED({pin: 13}, function(err, LED) {
+            if (err) {
+              $('.error').removeClass('hide');
+              return console.log(err);
+            }
+            that.led = LED;
+            that.led.blink(500);
+            that.led.on('change', function(data) {
+              if (data.mode == '000') {
+                $('#e2-status').removeClass('label-success');
+                $('#e2-status').html('OFF');
+              } else {
+                $('#e2-status').addClass('label-success');
+                $('#e2-status').html('ON');
+              }
+            });
+          });
+        } else {
+          that.led.blink(interval);
+        }
+      }
+    }
+  }
+
   $(document).ready(function(e) {
 
     // Listen for button click.
@@ -10,44 +50,11 @@ require(["jquery"], function($) {
       $('#e2-exampleConnection .alert-info').html('Trying to connect to your Arduino…');
 
       var that = this;
-      require(['scripts/libs/Noduino.js', 'scripts/libs/Noduino.Socket.js', 'scripts/libs/Logger.js'], function(NoduinoObj, Connector, Logger) {
-        var Noduino = new NoduinoObj({debug: false, host: 'http://localhost:8090'}, Connector, Logger);
-        Noduino.connect(function(err, board) {
-          $('#e2-exampleConnection .alert').addClass('hide');
-          if (err) {
-            $('.board-error').removeClass('hide');
-            console.log("Error connecting to board.");
-          } else {
-            console.log("Success connecting to board.");
-
-            // Start blinking.
-            var that = this;
-            if (!that.led) {
-              board.withLED({pin: 13}, function(err, LED) {
-                if (err) {
-                  $('.error').removeClass('hide');
-                  return console.log(err);
-                }
-
-                that.led = LED;
-                that.led.blink(500);
-                that.led.on('change', function(data) {
-                  if (data.mode == '000') {
-                    $('#e2-status').removeClass('label-success');
-                    $('#e2-status').html('OFF');
-                  } else {
-                    $('#e2-status').addClass('label-success');
-                    $('#e2-status').html('ON');
-                  }
-                });
-              });
-            } else {
-              that.led.blink(interval);
-            }
-
-
-          }
-        });
+      require(
+        ['scripts/libs/Noduino.js', 'scripts/libs/Noduino.Socket.js', 'scripts/libs/Logger.js'],
+        function( Noduino, Connector, Logger ) {
+          var noduinoConnection = new Noduino({debug: false, host: 'http://localhost:8090'}, Connector, Logger);
+          noduinoConnection.connect( getOnBoardConnectionCallback() );
       });
 
     });

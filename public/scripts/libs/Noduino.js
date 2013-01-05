ffccba0d58ee092af0c1a2c8b09c720ed53597a6
Left Field Labs <lfl@edam.local>
2013-01-04 17:32:43 -0800

    Formatted Noduino.js a bit.

--- public/scripts/libs/Noduino.js
+++ public/scripts/libs/Noduino.js
@@ -1,22 +1,23 @@
-define(['./Board.js'], function (objBoard) {
-  /** 
+define(['./Board.js'], function ( Board ) {
+  /**
    * Create Noduini object for handling general access
    * @param object options
    */
-  function Noduino(options, Connector, Logger) { 
+  function Noduino(options, NoduinoClientFacade, Logger) {
+    // TODO: What is this ridiculous pattern? A singleton or something?
     if (false === (this instanceof Noduino)) {
       return new Noduino(options); }
-      
-    this.c            = new Connector(options)
+
+    this.clientFacade            = new NoduinoClientFacade(options)
     this.logger       = new Logger(options);
-    this.options      = options;    
-    this.options.type = this.c.connection;
-    this.connected    = false;    
-    
+    this.options      = options;
+    this.options.type = this.clientFacade.connection;
+    this.connected    = false;
+
     this.setLoggerOptions();
     this.setLogger();
   };
-  
+
   Noduino.prototype.setLoggerOptions = function() {
     if (!this.options.logger) {
       return; }
@@ -25,41 +26,29 @@ define(['./Board.js'], function (objBoard) {
   };
 
   Noduino.prototype.setLogger = function() {
-    this.c.setLogger(this.logger);
+    this.clientFacade.setLogger(this.logger);
   };
 
   Noduino.prototype.log = function(level, msg) {
-    this.c.log(level, msg);
-  }
-
-  Noduino.prototype.write = function(data) {
-    this.c.write(data);
-  }
-  
-  Noduino.prototype.digitalRead = function(pin) {
-    this.c.digitalRead(pin);
-  }
-
-  Noduino.prototype.watchAnalogIn = function(pin, callback) {
-    this.c.watchAnalogIn(pin, callback);
-  }
-  
-  Noduino.prototype.watchDigitalIn = function(pin, callback) {
-    this.c.watchDigitalIn(pin, callback);
+    this.clientFacade.log(level, msg);
   }
 
   Noduino.prototype.connect = function(options, callback) {
     this.log('connecting to noduino');
-    
-    if (!callback) {
-      callback = options; options = {}; }
+
+    if ( !callback ) {
+      callback = options;
+      options = {};
+    }
     var that = this;
-    this.c.connect(options, function(err, board) {
-      if (err) { return callback(err); }    
+    this.clientFacade.connect( options, function( err, board ) {
+      if ( err ) {
+        return callback( err );
+      }
       that.connected = true;
-      callback(null, new objBoard(options, that.c));
+      callback( null, new Board( options, that.clientFacade ) );
     });
   }
-  
+
   return Noduino;
 });
